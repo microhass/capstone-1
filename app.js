2de0b8618f08fad6ba4c6e138d910bed7453f19f
hassanShakur <hass.shakurhassan@gmail.com>
2023-05-22 14:38:06 +0300

    Mobile menu design and implementation

---
+++ app.js
@@ -0,0 +1,18 @@
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+const backdrop = document.querySelector('.backdrop');
+const menu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', () => {
+  menu.classList.add('open-menu');
+});
+
+[closeIcon, backdrop].forEach((icon) =>
+  icon.addEventListener('click', () =>
+    menu.classList.remove('open-menu')
+  )
+);
+
+// closeIcon.addEventListener('click', () => {
+//   menu.classList.remove('open-menu');
+// });
